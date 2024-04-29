15410b11f333db48b5bd6caab05f288e4826e6fd
Al3ksemduble <ah097786@gmail.com>
2024-04-29 15:27:17 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Sistema_Escolar
+{
+    partial class FrmSystem
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNota1 = new System.Windows.Forms.TextBox();
+            this.txtNota2 = new System.Windows.Forms.TextBox();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnVerif = new System.Windows.Forms.Button();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblInstrucao1 = new System.Windows.Forms.Label();
+            this.lblInstrucao2 = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblMedia = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtNota1
+            // 
+            this.txtNota1.Location = new System.Drawing.Point(206, 81);
+            this.txtNota1.Name = "txtNota1";
+            this.txtNota1.Size = new System.Drawing.Size(202, 20);
+            this.txtNota1.TabIndex = 0;
+            // 
+            // txtNota2
+            // 
+            this.txtNota2.Location = new System.Drawing.Point(206, 133);
+            this.txtNota2.Name = "txtNota2";
+            this.txtNota2.Size = new System.Drawing.Size(202, 20);
+            this.txtNota2.TabIndex = 1;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(160, 250);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(121, 31);
+            this.btnLimpar.TabIndex = 2;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnVerif
+            // 
+            this.btnVerif.Location = new System.Drawing.Point(287, 250);
+            this.btnVerif.Name = "btnVerif";
+            this.btnVerif.Size = new System.Drawing.Size(121, 31);
+            this.btnVerif.TabIndex = 3;
+            this.btnVerif.Text = "Verificar";
+            this.btnVerif.UseVisualStyleBackColor = true;
+            this.btnVerif.Click += new System.EventHandler(this.btnVerif_Click);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Location = new System.Drawing.Point(12, 407);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(121, 31);
+            this.btnFechar.TabIndex = 4;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblMedia);
+            this.groupBox1.Controls.Add(this.lblResult);
+            this.groupBox1.Location = new System.Drawing.Point(35, 287);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(373, 100);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            // 
+            // lblInstrucao1
+            // 
+            this.lblInstrucao1.AutoSize = true;
+            this.lblInstrucao1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblInstrucao1.Location = new System.Drawing.Point(53, 82);
+            this.lblInstrucao1.Name = "lblInstrucao1";
+            this.lblInstrucao1.Size = new System.Drawing.Size(143, 17);
+            this.lblInstrucao1.TabIndex = 6;
+            this.lblInstrucao1.Text = "Digite a primeira nota";
+            // 
+            // lblInstrucao2
+            // 
+            this.lblInstrucao2.AutoSize = true;
+            this.lblInstrucao2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblInstrucao2.Location = new System.Drawing.Point(49, 134);
+            this.lblInstrucao2.Name = "lblInstrucao2";
+            this.lblInstrucao2.Size = new System.Drawing.Size(147, 17);
+            this.lblInstrucao2.TabIndex = 7;
+            this.lblInstrucao2.Text = "Digite a segunda nota";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblResult.Location = new System.Drawing.Point(6, 70);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(72, 17);
+            this.lblResult.TabIndex = 8;
+            this.lblResult.Text = "Resultado";
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblTitulo.Location = new System.Drawing.Point(9, 9);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(269, 20);
+            this.lblTitulo.TabIndex = 8;
+            this.lblTitulo.Text = "Digite as duas notas do aluno abaixo";
+            // 
+            // lblMedia
+            // 
+            this.lblMedia.AutoSize = true;
+            this.lblMedia.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblMedia.Location = new System.Drawing.Point(6, 16);
+            this.lblMedia.Name = "lblMedia";
+            this.lblMedia.Size = new System.Drawing.Size(105, 17);
+            this.lblMedia.TabIndex = 9;
+            this.lblMedia.Text = "Média do aluno";
+            // 
+            // FrmSystem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ClientSize = new System.Drawing.Size(435, 450);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.lblInstrucao2);
+            this.Controls.Add(this.lblInstrucao1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnVerif);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.txtNota2);
+            this.Controls.Add(this.txtNota1);
+            this.Name = "FrmSystem";
+            this.Text = "Sistema Escolar";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNota1;
+        private System.Windows.Forms.TextBox txtNota2;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnVerif;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label lblInstrucao1;
+        private System.Windows.Forms.Label lblInstrucao2;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblMedia;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema_Escolar
+{
+    public partial class FrmSystem : Form
+    {
+        public FrmSystem()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerif_Click(object sender, EventArgs e)
+        {
+            //Declarando variáveis
+            Double nota1, nota2, media;
+            nota1 = Convert.ToDouble(txtNota1.Text);
+            nota2 = Convert.ToDouble(txtNota2.Text);
+            
+            //Calculando a média das notas
+            media = (nota1 + nota2) / 2;
+
+            //Mostrando a média do aluno
+            lblMedia.Text = "A média do aluno é de: " + media;
+
+            //Aprovado/Reprovado
+            if (media >= 6.0)
+            {
+                lblResult.Text = "Aluno Aprovado!";
+            }
+            else
+            {
+                lblResult.Text = "Aluno Reprovado!";
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            //Limpando campos
+            txtNota1.Clear();
+            txtNota2.Clear();
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            //Fecha o programa
+            Close();
+        }
+    }
+}
